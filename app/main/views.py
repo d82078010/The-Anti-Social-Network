77faf7a54bc198d533809acfd03481c1c32f7993
Bruno Macabeus Aquino <brunomacabeusaquino@Brunos-MacBook-Pro.local>
2016-10-06 11:11:47 -0300

    Adicionado botão de remover posts

--- app/main/views.py
+++ app/main/views.py
@@ -183,6 +183,20 @@ def edit(id):
     form.body.data = post.body
     return render_template('edit_post.html', form=form)
 
+# delete post
+@main.route('/postdelete/<int:id>')
+@login_required
+def postdelete(id):
+    post = Post.query.filter_by(id=id)
+
+    if post.first().group is None:
+        go_to = redirect(url_for('.index'))
+    else:
+        go_to = redirect(url_for('.group', id=post.first().group_id))
+
+    post.delete()
+
+    return go_to
 
 # following stuff
 
